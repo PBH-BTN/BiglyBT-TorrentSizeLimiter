c0bab8aac751e917fd75d3c201c72d98548f7a30
Ghost_chu <ghostchu@111.com>
2026-03-20 23:25:17 +0800

    17

--- src/main/java/com/ghostchu/biglyplug/torrentsizelimiter/TelegramHook.java
+++ src/main/java/com/ghostchu/biglyplug/torrentsizelimiter/TelegramHook.java
@@ -29,6 +29,6 @@ public class TelegramHook {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }, Executors.newVirtualThreadPerTaskExecutor());
+        });
     }
 }
